c425b05c1f705461099c57c25285a6c121de2170
Dylan Best <best.dylan15@gmail.com>
2020-10-13 08:28:50 -0400

    updated seed dotenv path

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,8 @@
+require("dotenv").config({ path: `${__dirname}/../.env`});
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
